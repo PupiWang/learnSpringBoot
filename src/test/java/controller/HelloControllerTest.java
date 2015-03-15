f834133a9c9e5fb1661ca97a400949b799ea60ed
Pupi <dreamjl@live.cn>
2015-03-15 18:15:39 +0800

    add test for hello controller.

--- src/test/java/controller/HelloControllerTest.java
+++ src/test/java/controller/HelloControllerTest.java
@@ -1,7 +1,13 @@
 package controller;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HelloControllerTest {
-
+    @Test
+    public void should_not_return_null() {
+        String result = new HelloController().home();
+        assertNotNull(result);
+    }
 }
